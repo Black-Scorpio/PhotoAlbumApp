db9c470234d2abeadb9be66fc251811644e55ee5
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-04-28 15:40:54 -0400

    Icon made for scene

---
+++ src/main/java/com/example/photoalbumapp/Main.java
@@ -0,0 +1,28 @@
+package com.example.photoalbumapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        //Setting up the icon for the scene
+        Image icon = new Image(String.valueOf(getClass().getResource("images/cameralogo.png")));
+        stage.getIcons().add(icon);
+
+        stage.setTitle("Photo Album");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
